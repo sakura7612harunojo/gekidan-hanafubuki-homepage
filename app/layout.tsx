301fe9885f835f14ababb971cfd7a69f494ca086
sakura7612harunojo <h.sakura.7612@gmail.com>
2026-09-07 23:35:17 +0900

    feat: 桜の花びらアニメーションを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import GoogleAnalytics from "./google-analytics";
 
-import SakuraPetals from "@/components/SakuraPetals";
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.gekidan-hanafubuki.com"),
   title: { default: "劇団花吹雪｜大衆演劇 公式サイト", template: "%s｜劇団花吹雪" },
@@ -50,7 +49,6 @@ export default function RootLayout({
             __html: JSON.stringify(websiteJsonLd),
           }}
         />
-        <SakuraPetals />
         <GoogleAnalytics />
 {children}        <BackToTopButton />
       </body>
